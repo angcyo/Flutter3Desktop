f73f0b332c71e7d2b948d0a8912922629ea95aa5
angcyo <angcyo@126.com>
2024-12-25 18:32:32 +0800

    init core

--- flutter3_desktop_core/lib/flutter3_desktop_core.dart
+++ flutter3_desktop_core/lib/flutter3_desktop_core.dart
@@ -1,5 +1,14 @@
 library;
 
+import 'package:flutter/material.dart';
+import 'package:flutter3_basics/flutter3_basics.dart';
+import 'package:window_manager/window_manager.dart';
+
+export 'package:flutter3_basics/flutter3_basics.dart';
+export 'package:flutter3_widgets/flutter3_widgets.dart';
+
+part 'src/window/window_ex.dart';
+
 ///
 /// @author <a href="mailto:angcyo@126.com">angcyo</a>
 /// @since 2024-12-25
---
+++ flutter3_desktop_core/lib/src/window/window_ex.dart
@@ -0,0 +1,65 @@
+part of '../../flutter3_desktop_core.dart';
+
+///
+/// @author <a href="mailto:angcyo@126.com">angcyo</a>
+/// @date 2024/12/25
+///
+
+/// 初始化窗口
+/// 请在[runApp]之前调用
+///
+/// 默认的尺寸和位置在 `main.cpp`
+/// ```
+/// FlutterWindow window(project);
+/// Win32Window::Point origin(10, 10);
+/// Win32Window::Size size(1280, 720);
+/// ```
+///
+/// [skipTaskbar] 窗口是否在任务栏中隐藏
+///
+/// [DragToMoveArea]
+/// [DragToResizeArea]
+/// [WindowCaption]
+///  - [WindowCaptionButton.minimize]
+///  - [WindowCaptionButton.maximize]
+///  - [WindowCaptionButton.unmaximize]
+///  - [WindowCaptionButton.close]
+///
+/// [WindowListener]
+///
+@initialize
+Future initWindow({
+  Size? size /*Size(800, 600) Size(1280, 720)*/,
+  bool? center = true,
+  Size? minimumSize,
+  Size? maximumSize,
+  bool? alwaysOnTop,
+  bool? fullScreen,
+  Color? backgroundColor = Colors.transparent,
+  bool? skipTaskbar = false,
+  String? title,
+  TitleBarStyle? titleBarStyle = TitleBarStyle.normal,
+  bool? windowButtonVisibility,
+}) async {
+  ensureInitialized();
+  // 必须加上这一行。
+  await windowManager.ensureInitialized();
+
+  final windowOptions = WindowOptions(
+    size: size,
+    center: center,
+    minimumSize: minimumSize,
+    maximumSize: maximumSize,
+    alwaysOnTop: alwaysOnTop,
+    fullScreen: fullScreen,
+    backgroundColor: backgroundColor,
+    skipTaskbar: skipTaskbar,
+    title: title,
+    titleBarStyle: titleBarStyle,
+    windowButtonVisibility: windowButtonVisibility,
+  );
+  windowManager.waitUntilReadyToShow(windowOptions, () async {
+    await windowManager.show();
+    await windowManager.focus();
+  });
+}
