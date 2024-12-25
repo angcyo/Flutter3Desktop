5ae41a34cdface57824d0cfbdc1cd8e58a3f8581
angcyo <angcyo@126.com>
2024-12-25 15:55:28 +0800

    init

---
+++ flutter3_desktop_app/lib/flutter3_desktop_app.dart
@@ -0,0 +1,9 @@
+library;
+
+export 'package:flutter3_desktop_core/flutter3_desktop_core.dart';
+
+
+///
+/// @author <a href="mailto:angcyo@126.com">angcyo</a>
+/// @since 2024-12-25
+///
---
+++ flutter3_desktop_core/lib/flutter3_desktop_core.dart
@@ -0,0 +1,6 @@
+library;
+
+///
+/// @author <a href="mailto:angcyo@126.com">angcyo</a>
+/// @since 2024-12-25
+///
