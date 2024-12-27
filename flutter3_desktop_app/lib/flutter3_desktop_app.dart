133fd3d17f3c85ffae86431f7e4a695617f39885
angcyo <angcyo@126.com>
2024-12-27 14:40:33 +0800

    + flutter3 app

--- flutter3_desktop_app/lib/flutter3_desktop_app.dart
+++ flutter3_desktop_app/lib/flutter3_desktop_app.dart
@@ -1,7 +1,7 @@
 library;
 
 export 'package:flutter3_desktop_core/flutter3_desktop_core.dart';
-
+export 'package:flutter3_app/flutter3_app.dart';
 
 ///
 /// @author <a href="mailto:angcyo@126.com">angcyo</a>
